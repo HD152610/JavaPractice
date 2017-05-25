84593d3b216ab332ff9ded1a8b4e8187d903b02f
nth12 <nth12@DESKTOP-0KIETI1>
2017-05-25 16:27:23 +0900

    자바 수행평가

---
+++ JavaPractice/src/org/dimigo/interfaces/DBtest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package org.dimigo.interfaces;
+
+/**
+ * <pre>
+ * org.dimigo.interfaces
+ *   |_ DBtest
+ *
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 5. 25.
+ * </pre>
+ *
+ * @author		: nth12
+ * @version		: 1.0
+ */
+public class DBtest {
+
+	
+	public static void main(String[] args) {
+		IDBManager[] database = {
+			IDBManager.getDBObject(IDBManager.SYBASE_DATABASE),	
+			IDBManager.getDBObject(IDBManager.ORACLE_DATABASE)	
+		};
+		System.out.println("<<변경 전>>");
+		crud(database[0]);
+		System.out.println("");
+		System.out.println("<<변경 후>>");
+		crud(database[1]);
+	}
+	
+	public static void crud(IDBManager db){
+		db.insert();
+		db.search();
+		db.update();
+		db.delete();
+	}
+
+}
